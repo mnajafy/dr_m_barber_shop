28fe555d003e71d415c0336c807cb5cedd0cf650
m.najafy <m.najafy@hotmail.com>
2020-06-02 14:16:04 +0200

    Add core/Autoloader.php

---
+++ core/Autoloader.php
@@ -0,0 +1,16 @@
+<?php
+
+class Autoloader 
+{
+    static function register()
+    {
+        spl_autoload_register([__CLASS__, 'autoload']);
+    }
+
+    static function autoload($class_name)
+    {
+        require $class_name . '.php';
+    }
+}
+
+?>
