41176fe4671eb373f2bfd93cc1ac78613c56e8e6
mnajafy <64306946+mnajafy@users.noreply.github.com>
2020-08-06 01:59:28 +0200

    Update style Form

--- app/views/admin/gallery-index.php
+++ app/views/admin/gallery-index.php
@@ -8,28 +8,22 @@ $this->title                  = 'asd';
 $this->params['breadcrumb'][] = ['label' => 'Home', 'url' => ['home/index']];
 $this->params['breadcrumb'][] = $this->title;
 ?>
-<div class="container">
-    <?php $form  = ActiveForm::begin(['options' => ['style' => 'padding: 15px;']]) ?>
-    <div class="form-group">
-        <label for="demo">Demo</label>
-        <input type="text" class="form-control" name="demo" id="demo">
-        <small class="form-text">We'll never share your email with anyone else.</small>
-    </div>
-    <button class="btn btn-brown">Demo</button>
-    <div class="row">
-        <div class="col-4">
-            <?= $form->field($model, 'username') ?>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-4">
-            <?= $form->field($model, 'password') ?>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-4">
-            <?= Html::submitButton('Login', ['class' => 'btn btn-brown']) ?>
-        </div>
-    </div>
-    <?php ActiveForm::end() ?>
-</div>
+
+<section class="section">
+    <div class="container">
+        <div class="section-title center">
+            <h4>WELCOME TO THE BOARD</h4>
+            <h3>LOGIN</h3>
+            <hr>
+        </div> <!-- end section title -->
+        <?php $form  = ActiveForm::begin(['options' => ['style' => 'padding: 15px;']]) ?>
+        <div class="row">
+            <div class="col-4 mx-auto">
+                <?= $form->field($model, 'username') ?>
+                <?= $form->field($model, 'password') ?>
+                <?= Html::submitButton('Login', ['class' => 'btn btn-brown']) ?>
+            </div>
+        </div> <!-- end row -->
+        <?php ActiveForm::end() ?>
+    </div> <!-- end container -->
+</section> <!-- end section -->
