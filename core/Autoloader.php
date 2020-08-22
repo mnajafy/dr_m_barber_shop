df4db1cc5ff63d97cd3044be6c317c0fad8db337
Hossein Najafi <hnajafi1994@gmail.com>
2020-08-23 00:07:12 +0430

    Add TODO

--- core/Autoloader.php
+++ core/Autoloader.php
@@ -1,7 +1,7 @@
 <?php
 class Autoloader {
     static function register() {
-        spl_autoload_register([__CLASS__, 'autoload']);
+        spl_autoload_register([__CLASS__, 'autoload'], true, true);
     }
     static function autoload($name) {
         $filename = str_replace('\\', '/', $name) . '.php';
